49f90f6b9a1b51c4d31be93af8ec8531b51dffaf
sorious77 <sorious77@naver.com>
2022-09-05 22:42:29 +0900

    #0.2 Server Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Woom
+
+Zoom Clone using NodeJS, WebRTC and Websockets.
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is listen at localhost:3000 🚀");
+});
